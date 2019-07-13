a8161522f05eb1c4815784883cc641e065f89836
suibaka <suibaka@hotmail.co.jp>
2019-07-13 15:41:15 +0900

    feat: add root value to union find

--- src/unionfind.ml
+++ src/unionfind.ml
@@ -1,12 +1,15 @@
 
-type 'a root = { mutable rank: int; }
+type 'a root = {
+  mutable value: 'a;
+  mutable rank: int;
+}
 
 type 'a t = { mutable parent : 'a node; }
 and 'a node =
     Root of 'a root (* it is root (that is, representitive) *)
   | Parent of 'a t  (* it has parent node *)
 
-let create () = { parent = Root { rank = 0; }}
+let create v = { parent = Root { value = v; rank = 0; }}
 
 (* compress path to root *)
 let compress t =
@@ -33,18 +36,25 @@ let root t = match (representitive t).parent with
   | Root r -> r
   | _ -> failwith "Unionfind structure is broken"
 
+let get_value t = (root t).value
+
 let is_same t1 t2 = (root t1) == (root t2)
 
-let union t1 t2 =
+(*
+ * @param merge_f : new root value function when merge r1 and r2
+ *)
+let union t1 t2 merge_f =
   let r1 = representitive t1 in
   let r2 = representitive t2 in
   if r1 == r2 then ()
   else
     let rt1 = root r1 in
     let rt2 = root r2 in
-    if rt1.rank < rt2.rank then
-      r1.parent <- Parent r2
-    else begin
+    if rt1.rank < rt2.rank then begin
+      r1.parent <- Parent r2;
+      rt2.value <- merge_f rt2.value rt1.value
+    end else begin
       r2.parent <- Parent r1;
+      rt1.value <- merge_f rt1.value rt2.value;
       if rt1.rank = rt2.rank then rt1.rank <- rt1.rank + 1;
     end
--- tests/unionfind_test.ml
+++ tests/unionfind_test.ml
@@ -2,10 +2,11 @@ open OUnit2
 open Miniml
 
 let union_find_test _ =
-  let t1 = Unionfind.create () in
-  let t2 = Unionfind.create () in
-  let t3 = Unionfind.create () in
-  Unionfind.union t1 t2;
+  let merge_f = (fun x _ -> x) in
+  let t1 = Unionfind.create 0 in
+  let t2 = Unionfind.create 1 in
+  let t3 = Unionfind.create 2 in
+  Unionfind.union t1 t2 merge_f;
   assert_equal (Unionfind.is_same t1 t2) true;
   assert_equal (Unionfind.is_same t1 t3) false;
   assert_equal (Unionfind.is_same t2 t3) false;
@@ -14,22 +15,26 @@ let union_find_test _ =
   assert_equal (Unionfind.root t3).rank 0;
 
   let t4 = t2 in
-  Unionfind.union t4 t3;
+  Unionfind.union t4 t3 merge_f;
   assert_equal (Unionfind.is_same t1 t3) true;
 
-  let t1 = Unionfind.create () in
-  let t2 = Unionfind.create () in
-  let t3 = Unionfind.create () in
-  let t4 = Unionfind.create () in
-  Unionfind.union t1 t2;
-  Unionfind.union t3 t4;
-  Unionfind.union t2 t4;
+  let t1 = Unionfind.create 0 in
+  let t2 = Unionfind.create 1 in
+  let t3 = Unionfind.create 2 in
+  let t4 = Unionfind.create 3 in
+  Unionfind.union t1 t2 merge_f;
+  Unionfind.union t3 t4 merge_f;
+  Unionfind.union t2 t4 merge_f;
   assert_equal (Unionfind.root t1) (Unionfind.root t2);
   assert_equal (Unionfind.root t1) (Unionfind.root t3);
   assert_equal (Unionfind.root t1) (Unionfind.root t4);
   assert_equal (Unionfind.root t2) (Unionfind.root t3);
   assert_equal (Unionfind.root t2) (Unionfind.root t4);
-  assert_equal (Unionfind.root t3) (Unionfind.root t4)
+  assert_equal (Unionfind.root t3) (Unionfind.root t4);
+  assert_equal (Unionfind.get_value t1) 0;
+  assert_equal (Unionfind.get_value t2) 0;
+  assert_equal (Unionfind.get_value t3) 0;
+  assert_equal (Unionfind.get_value t4) 0
 
 let suite =
   "suite">:::["test1">:: union_find_test]
